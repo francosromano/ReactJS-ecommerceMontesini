9c0e091bff1ea354ac9628d0d4b82bd7a860b3b8
francosromano <francosromano.1994@gmail.com>
2024-04-24 12:59:38 -0300

    Proyecto final

--- src/Firebase/Firebase.jsx
+++ src/Firebase/Firebase.jsx
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG)
+console.log(firebaseConfig)
 
 const app = initializeApp(firebaseConfig)
 
