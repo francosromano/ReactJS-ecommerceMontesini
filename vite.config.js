bf13750ecb5b83f5c47aab6d5c74cdf2c187fcce
francosromano <francosromano.1994@gmail.com>
2024-04-24 14:15:17 -0300

    Proyecto final

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': process.env,
+  },
 })
