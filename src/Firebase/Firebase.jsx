834b612b9f14ad3cd2ee0e1834cc8d71c438b80b
francosromano <francosromano.1994@gmail.com>
2024-04-24 14:56:13 -0300

    Proyecto final

--- src/Firebase/Firebase.jsx
+++ src/Firebase/Firebase.jsx
@@ -1,27 +1,31 @@
+// # Haciendo esto, me carga el Firestore pero no el html al hosting
 import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 
-// const firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG)
-/* eslint-disable no-undef */
-const firebaseConfig= {
-    apiKey: process.env.VITE_FIREBASE_API_KEY,
-    authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.VITE_FIREBASE_APP_ID,
-}
+const firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG)
+
 const app = initializeApp(firebaseConfig)
 
 export const database = getFirestore(app)
 
 
+
+// Haciendo esto, me carga el html al hosting, pero no me carga el Firestore: 
+
+// /* eslint-disable no-undef */
 // import { initializeApp } from "firebase/app"
 // import { getFirestore } from "firebase/firestore"
 
 // // const firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG)
 
-
+// const firebaseConfig= {
+//     apiKey: process.env.VITE_FIREBASE_API_KEY,
+//     authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+//     projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+//     appId: process.env.VITE_FIREBASE_APP_ID,
+// }
 
 // const app = initializeApp(firebaseConfig)
 
--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,22 @@
+// Haciendo esto, me carga el Firestore pero no el html al hosting
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
+export default defineConfig({
+  plugins: [react()],
+})
 
 
+
+// Haciendo esto, me carga el html al hosting, pero no me carga el Firestore: 
 /* eslint-disable no-undef */
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react-swc'
 
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env': process.env,
-  },
-})
+// export default defineConfig({
+//   plugins: [react()],
+//   define: {
+//     'process.env': process.env,
+//   },
+// })
 
